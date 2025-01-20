51d7ced4620dc7b53b5d4f2b9a71eb7305d20dcf
Charles32-Dev <Charles_fanautico@hotmail.com>
2025-01-20 00:58:50 -0300

    alteração no js

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ function generaterNumber() {
     const min = Math.ceil(document.querySelector(".input1").value)
     const max = Math.floor(document.querySelector(".input2").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (max <= min) {
+        alert("Você tem que colocar o VALOR maior que o minimo!")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(result)
+    }
 
-    alert(result)
+
+
+
 
 }
